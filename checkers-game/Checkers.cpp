75db49c557789ccbbf6183a0def7b63bc0a7f974
Mia Patrikios <mpatrikios@icloud.com>
2024-05-01 14:58:42 -0400

    commit

--- checkers-game/Checkers.cpp
+++ checkers-game/Checkers.cpp
@@ -56,15 +56,19 @@ void Checkers::printBoard(){
         case 1: //FR
         Board[i+1][j+1] = Board[i][j];
         Board[i][j] = ' ';
+        break;
         case 2: //FL
         Board[i+1][j-1] = Board[i][j];
         Board[i][j] = ' ';
+        break;
         case 3: //BL
         Board[i-1][j-1] = Board[i][j];
         Board[i][j] = ' ';
+        break;
         case 4: //BR
         Board[i-1][j+1] = Board[i][j];
         Board[i][j] = ' ';
+        break;
     }
 
     }
@@ -79,6 +83,7 @@ void Checkers::printBoard(){
         Board[i+2][j+2] = Board[i][j];
         Board[i+1][j+1] = ' ';
         Board[i][j] = ' ';
+        break;
         case 6: //JFL
         if (Board[i][j] == 'X') {
             score1++;
@@ -86,6 +91,7 @@ void Checkers::printBoard(){
         Board[i+2][j-2] = Board[i][j];
         Board[i+1][j-1] = ' ';
         Board[i][j] = ' ';
+        break;
         case 7: //JBL
         if (Board[i][j] == 'X') {
             score1++;
@@ -93,6 +99,7 @@ void Checkers::printBoard(){
         Board[i-2][j-2] = Board[i][j];
         Board[i-1][j-1] = ' ';  
         Board[i][j] = ' ';
+        break;
         case 8: //JBR
         if (Board[i][j] == 'X') {
             score1++;
@@ -100,6 +107,7 @@ void Checkers::printBoard(){
         Board[i-2][j+2] = Board[i][j];
         Board[i-1][j+1] = ' ';
         Board[i][j] = ' ';
+        break;
     }
   }
   
@@ -126,21 +134,26 @@ bool Checkers::isValidMove(int i, int j, int direction){
     switch(direction){
         case 1: //FR
         if (Board[i+1][j+1] == ' ') return true;
+        break;
         case 2: //FL
         if (Board[i+1][j-1] == ' ') return true;
+        break;
         case 3: //BL
         if (Board[i][j] != 'K') {
             cout << "Not a King. Cannot move backwards.";
             return false;
         }if (Board[i-1][j-1] == ' ') return true;
+        break;
         case 4: //BR
         if (Board[i][j] != 'K') {
             cout << "Not a King. Cannot move backwards.";
             return false;
         }if (Board[i-1][j+1] == ' ') return true;
+        break;
         default:
         cout << "Invalid move. Try again.";
         return false;
+        break;
   } 
   }
 
@@ -148,20 +161,25 @@ bool Checkers::isValidJump(int i, int j, int direction){
     switch(direction){
        case 5: //JFR
         if (Board[i+1][j+1] == 'X' && Board[i+2][j+2] == ' ') return true;
+        break;
         case 6: //JFL
         if (Board[i+1][j-1] == 'X' && Board[i+2][j-2] == ' ') return true;
+        break;
         case 7: //JBL
         if (Board[i][j] != 'K') {
             cout << "Not a King. Cannot move backwards.";
             return false;
         }if (Board[i-1][j-1] == 'X' && Board[i-2][j-2] == ' ') return true;
+        break;
         case 8: //JBR
         if (Board[i][j] != 'K') {
             cout << "Not a King. Cannot move backwards.";
             return false;
         }if (Board[i-1][j+1] == 'X' && Board[i-2][j+2] == ' ') return true;
+        break;
         default:
         cout << "Invalid jump. Try again.";
         return false;
+        break;
     }
 }
